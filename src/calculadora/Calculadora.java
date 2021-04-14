da8ee84c6b84df1b3005e0ea27b087efef3926f6
axeldos <axelabalosss@gmail.com>
2021-04-14 00:29:17 -0300

    se agregon un txt con las respueestas y se crearon metodos

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -4,6 +4,39 @@ import java.util.Scanner;
  
 public class Calculadora { 
 	
+	public static void sumar(Integer a, Integer b) {
+		
+case 1: resultado = a + b;
+	
+	System.out.println("El resultado es " + resultado.toString());
+		
+	}
+	
+	
+	public static void restar(Integer a, Integer b) {
+		
+case 1: resultado = a - b;
+	
+	System.out.println("El resultado es " + resultado.toString());
+		
+	}
+	
+	
+	public static void dividir(Integer a, Integer b) {
+		
+case 1: resultado = a / b;
+	
+	System.out.println("El resultado es " + resultado.toString());
+		
+	}
+	public static void multiplicar(Integer a, Integer b) {
+		
+case 1: resultado = a * b;
+	
+	System.out.println("El resultado es " + resultado.toString());
+		
+	}
+	
  public static void main(String[] args) {
 	 
 	 Integer a;  
@@ -13,7 +46,7 @@ public class Calculadora {
  	Scanner teclado = new Scanner(System.in);
  	
  	do{
- 		System.out.println("Ingrese su operacion /n 1 para multiplicar /n 2 para restar /n 3 para sumar /n 4 para dividir");
+ 		System.out.println("Ingrese su operacion /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
  		opcion= teclado.nextInt();
  	}while(opcion < 1 && opcion > 4);
  System.out.println("Ingrese el primer numero");
@@ -26,23 +59,17 @@ public class Calculadora {
  
  
  switch(opcion){
-	case 1: resultado = a * b;
-	
-	System.out.println("El resultado es " + resultado.toString());
+ sumar(a,b);
 		break;
 	case 2:
-		resultado = a - b;
-		System.out.println("El resultado es " + resultado.toString());
+		restar(a,b);
 		break;
 	case 3:		
-		resultado = a + b;
-		System.out.println("El resultado es " + resultado.toString());
-	
+		multiplicar(a,b);
 
 	break;
 	case 4:
-		resultado = a / b;
-		System.out.println("El resultado es " + resultado.toString());
+		dividir(a,b);
 }
  
 	
