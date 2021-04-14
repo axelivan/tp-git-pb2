8f3303d5f595df6309c2ba960ebc33812298a6a2
axeldos <axelabalosss@gmail.com>
2021-04-13 23:28:40 -0300

    se cambio el orden de las operaciones matematicas

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,8 +1,8 @@
 package calculadora;
 
 import java.util.Scanner;
-
-public class Calculadora {
+ 
+public class Calculadora { 
 	
  public static void main(String[] args) {
 	 
@@ -13,7 +13,7 @@ public class Calculadora {
  	Scanner teclado = new Scanner(System.in);
  	
  	do{
- 		System.out.println("Ingrese su operacion /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+ 		System.out.println("Ingrese su operacion /n 1 para multiplicar /n 2 para restar /n 3 para sumar /n 4 para dividir");
  		opcion= teclado.nextInt();
  	}while(opcion < 1 && opcion > 4);
  System.out.println("Ingrese el primer numero");
